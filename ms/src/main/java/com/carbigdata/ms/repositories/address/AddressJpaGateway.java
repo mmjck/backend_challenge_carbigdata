10a2bdcf20e6207141b00f2411f6105abfaa22a3
mmjck <msantosjck@gmail.com>
2024-07-27 17:43:41 -0400

    feat: added implementation of address gateway

---
+++ ms/src/main/java/com/carbigdata/ms/repositories/address/AddressJpaGateway.java
@@ -0,0 +1,59 @@
+package com.carbigdata.ms.repositories.address;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.carbigdata.ms.domain.address.entities.Address;
+import com.carbigdata.ms.domain.address.exceptions.AddressNotFoundException;
+import com.carbigdata.ms.domain.address.gateway.AddressGateway;
+import com.carbigdata.ms.repositories.address.jpa.AddressJpaRepository;
+import com.carbigdata.ms.repositories.address.jpa.mapper.Address2AddressJpaModel;
+import com.carbigdata.ms.repositories.address.jpa.mapper.AddressJpaModel2Address;
+import com.carbigdata.ms.repositories.address.jpa.model.AddressJpaModel;
+
+@Component
+public class AddressJpaGateway implements AddressGateway {
+
+    private final AddressJpaRepository repository;
+
+    public AddressJpaGateway(AddressJpaRepository repository) {
+        this.repository = repository;
+    }
+
+
+    @Override
+    public Address save(Address data) {
+
+        var entity = Address2AddressJpaModel.mapper(data);
+        var client = this.repository.save(entity);
+
+        return AddressJpaModel2Address.mapper(client);
+    }
+
+    @Override
+    public Address update(Address data) {
+        var entity = Address2AddressJpaModel.mapper(data);
+        var client = this.repository.save(entity);
+
+        return AddressJpaModel2Address.mapper(client);
+    }
+
+    @Override
+    public void delete(int id) {
+        this.repository.findById(id).orElseThrow(() -> new AddressNotFoundException());
+        this.repository.deleteById(id);
+    }
+
+    @Override
+    public Address findById(int id) {
+        Optional<AddressJpaModel> address = this.repository.findById(id);
+
+        if (address.isEmpty()) {
+            return null;
+        }
+
+        return AddressJpaModel2Address.mapper(address.get());
+    }
+
+}
