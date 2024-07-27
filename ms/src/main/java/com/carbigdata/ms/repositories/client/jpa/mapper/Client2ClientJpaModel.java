3a0403cabcd39b70fdb8b802acf0eaafab947152
mmjck <msantosjck@gmail.com>
2024-07-27 17:44:04 -0400

    feat: added mapper for transforming data referring to client

---
+++ ms/src/main/java/com/carbigdata/ms/repositories/client/jpa/mapper/Client2ClientJpaModel.java
@@ -0,0 +1,19 @@
+package com.carbigdata.ms.repositories.client.jpa.mapper;
+
+import java.util.function.Function;
+import com.carbigdata.ms.domain.client.entities.Client;
+import com.carbigdata.ms.repositories.client.jpa.model.ClientJpaModel;
+
+public class Client2ClientJpaModel implements Function<Client, ClientJpaModel> {
+
+    public static ClientJpaModel mapper(final Client client) {
+        return new Client2ClientJpaModel().apply(client);
+    }
+
+    @Override
+    public ClientJpaModel apply(Client client) {
+        return new ClientJpaModel(client.getId(), client.getFullName(), client.getCpf(), client.getPassword(),
+                client.getBirthDate(), client.getCreatedAt());
+    }
+
+}
---
+++ ms/src/main/java/com/carbigdata/ms/repositories/client/jpa/mapper/ClientJpaModel2Client.java
@@ -0,0 +1,24 @@
+package com.carbigdata.ms.repositories.client.jpa.mapper;
+
+
+import com.carbigdata.ms.domain.client.entities.Client;
+import com.carbigdata.ms.repositories.client.jpa.model.ClientJpaModel;
+
+import java.util.function.Function;
+
+public class ClientJpaModel2Client implements Function<ClientJpaModel, Client> {
+    public static Client mapper(final ClientJpaModel client) {
+        return new ClientJpaModel2Client().apply(client);
+    }
+
+    @Override
+    public Client apply(ClientJpaModel client) {
+        return new Client(
+            client.getId(), 
+                client.getFullName(), 
+                client.getCpf(),
+                client.getPassword(), 
+                client.getBirthDate(),
+                client.getCreatedAt());
+    }
+}
