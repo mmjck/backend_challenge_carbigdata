cf5d6aa40c8e03d3271b2cd785318ff36cb8fa94
mmjck <msantosjck@gmail.com>
2024-07-28 12:56:52 -0400

    feat: added dtos used in address controller

---
+++ ms/src/main/java/com/carbigdata/ms/controller/address/dto/UpdateAddressRequestDTO.java
@@ -0,0 +1,14 @@
+package com.carbigdata.ms.controller.address.dto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record UpdateAddressRequestDTO(
+    String street,
+    
+    @JsonProperty("zip_code")
+    String zipCode,
+    String city,
+    String state,
+    String district
+){
+
+}
