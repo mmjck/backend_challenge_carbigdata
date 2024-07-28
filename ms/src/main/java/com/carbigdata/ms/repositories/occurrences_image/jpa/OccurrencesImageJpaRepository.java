cbcd3e12132a65db8fbe3fba6a50a94184d077a7
mmjck <msantosjck@gmail.com>
2024-07-28 12:02:48 -0400

    feat: added occurrences images repository

---
+++ ms/src/main/java/com/carbigdata/ms/repositories/occurrences_image/jpa/OccurrencesImageJpaRepository.java
@@ -0,0 +1,10 @@
+package com.carbigdata.ms.repositories.occurrences_image.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.carbigdata.ms.repositories.occurrences_image.jpa.model.OccurrencesImageJpaModel;
+
+
+public interface OccurrencesImageJpaRepository extends JpaRepository<OccurrencesImageJpaModel, Integer> {
+   
+
+}
