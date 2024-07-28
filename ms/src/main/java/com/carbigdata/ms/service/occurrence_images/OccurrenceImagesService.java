4058a5e88838698744fbb7348eb709d810601326
mmjck <msantosjck@gmail.com>
2024-07-28 12:25:26 -0400

    feat: added occurrence images service

---
+++ ms/src/main/java/com/carbigdata/ms/service/occurrence_images/OccurrenceImagesService.java
@@ -0,0 +1,17 @@
+package com.carbigdata.ms.service.occurrence_images;
+
+import com.carbigdata.ms.domain.occurrences_image.entities.OccurrencesImage;
+import com.carbigdata.ms.domain.pagination.PaginationResponse;
+
+
+
+public interface OccurrenceImagesService {
+    public OccurrencesImage get(int id);
+
+    public PaginationResponse<OccurrencesImage> findAll(int page, int pageSize);
+    
+    public OccurrencesImage update(int id, String hash, String path);
+    public OccurrencesImage create(String hash,  String path, int occurenceId);
+
+    public void delete(int id);
+}
