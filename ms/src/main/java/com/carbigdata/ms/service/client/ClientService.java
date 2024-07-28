11b75d5d66ea06f074b2af7f3e6fef860abe5e4e
mmjck <msantosjck@gmail.com>
2024-07-28 12:21:19 -0400

    feat: added client service

---
+++ ms/src/main/java/com/carbigdata/ms/service/client/ClientService.java
@@ -0,0 +1,12 @@
+package com.carbigdata.ms.service.client;
+
+import java.time.LocalDateTime;
+import com.carbigdata.ms.domain.client.entities.Client;
+
+public interface ClientService {
+    public Client get(int id);
+    public Client findByCpf(String cpf);
+    public Client update(int id, String name, String cpf, LocalDateTime birthDate);
+
+    public void delete(int id);
+}
