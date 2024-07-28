f0d592f98424b48ee8091cfe25a5aba33114f51a
mmjck <msantosjck@gmail.com>
2024-07-28 12:29:48 -0400

    feat: added occurrence service

---
+++ ms/src/main/java/com/carbigdata/ms/service/occurrence/OccurrenceService.java
@@ -0,0 +1,20 @@
+package com.carbigdata.ms.service.occurrence;
+
+
+import java.util.List;
+import com.carbigdata.ms.domain.occurrences.entities.Occurrences;
+import com.carbigdata.ms.domain.occurrences.entities.OccurrencesStatus;
+
+public interface OccurrenceService {
+    public Occurrences get(Integer id);
+
+    public List<?> findAll(String fullName, String cpf, String city, Boolean orderByCity, Boolean orderByDate, String direction);
+    
+    public Occurrences update(Integer id, OccurrencesStatus status);
+    public Occurrences create(Integer addressId, Integer clientId);
+
+    public void delete(int id);
+
+    public void finished(int id);
+
+}
