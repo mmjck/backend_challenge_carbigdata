de454a5f342daed089db1ed329df78d08a76e3f5
mmjck <msantosjck@gmail.com>
2024-07-28 12:20:35 -0400

    feat: added address service impl

---
+++ ms/src/main/java/com/carbigdata/ms/service/address/impl/AddressServiceImpl.java
@@ -0,0 +1,84 @@
+package com.carbigdata.ms.service.address.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.carbigdata.ms.domain.address.entities.Address;
+import com.carbigdata.ms.domain.address.exceptions.AddressNotFoundException;
+import com.carbigdata.ms.domain.address.gateway.AddressGateway;
+import com.carbigdata.ms.service.address.AddressService;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    private final AddressGateway gateway;
+
+    private AddressServiceImpl(AddressGateway gateway) {
+        this.gateway = gateway;
+    }
+
+    public static AddressServiceImpl build(AddressGateway gateway) {
+        return new AddressServiceImpl(gateway);
+    }
+
+    @Override
+    public Address get(int id) {
+        return this.gateway.findById(id);
+    }
+
+    @Override
+    public Address create(
+            String state,
+            String city,
+            String zipCode,
+            String district) {
+
+        var client = Address
+                .builder()
+                .city(city)
+                .state(state)
+                .district(district)
+                .zipCode(zipCode)
+                .build();
+
+        return this.gateway.save(client);
+
+    }
+
+    @Override
+    public void delete(int id) {
+        this.gateway.delete(id);
+    }
+
+    @Override
+    public Address update(int id,
+            String state,
+            String city,
+            String zipCode,
+            String district) {
+        Address address = this.gateway.findById(id);
+
+        if (address == null) {
+            throw new AddressNotFoundException();
+        }
+            
+        if (state != null && !state.equals(address.getState())) {
+            address.setState(state);
+        }
+
+        if (city != null && city.equals(address.getCity())) {
+            address.setCity(city);
+        }
+
+        if (zipCode != null && zipCode.equals(address.getZipCode())) {
+            address.setZipCode(zipCode);
+        }
+
+        if (district != null && district.equals(address.getDistrict())) {
+            address.setDistrict(district);
+        }
+
+        return this.gateway.save(address);
+
+    }
+
+}
