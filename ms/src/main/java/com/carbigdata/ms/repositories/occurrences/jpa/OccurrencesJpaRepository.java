6a49ceb308b9dd0024b74b45a6b298c6b3f3812c
mmjck <msantosjck@gmail.com>
2024-07-28 12:17:06 -0400

    fead added query to list all occurrences with details

---
+++ ms/src/main/java/com/carbigdata/ms/repositories/occurrences/jpa/OccurrencesJpaRepository.java
@@ -0,0 +1,24 @@
+package com.carbigdata.ms.repositories.occurrences.jpa;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.carbigdata.ms.repositories.occurrences.jpa.mapper.OccurrencesListDTO;
+import com.carbigdata.ms.repositories.occurrences.jpa.model.OccurrencesJpaModel;
+
+
+import java.util.List;
+
+
+public interface OccurrencesJpaRepository extends JpaRepository<OccurrencesJpaModel, Integer>{
+     
+    @Query(name = "OccurrencesJpaModel.findAll", nativeQuery = true)
+    public List<OccurrencesListDTO> findWithDetails(
+            @Param("full_name") String fullName,
+            @Param("cpf") String cpf,
+            @Param("city") String city
+            
+    );
+}
