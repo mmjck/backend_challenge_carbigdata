4b4f4ae7e6c9eede0f50305d664caacef63611c3
mmjck <msantosjck@gmail.com>
2024-07-28 12:43:23 -0400

    feat: added dtos used in auth controller

---
+++ ms/src/main/java/com/carbigdata/ms/controller/auth/dto/AuthResponseDTO.java
@@ -0,0 +1,5 @@
+package com.carbigdata.ms.controller.auth.dto;
+
+public record AuthResponseDTO(
+        String token) {
+}
---
+++ ms/src/main/java/com/carbigdata/ms/controller/auth/dto/RegisterClientRequestDTO.java
@@ -0,0 +1,23 @@
+package com.carbigdata.ms.controller.auth.dto;
+
+
+
+import java.time.LocalDateTime;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record RegisterClientRequestDTO(
+    @NotNull String name,
+    @NotNull String password,
+    @NotNull String cpf,
+
+    @JsonProperty(value = "birth_date")
+    @NotNull 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss") 
+    LocalDateTime birthDate
+){
+
+}
