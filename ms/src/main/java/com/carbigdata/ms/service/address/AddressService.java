0fbcc6cedd0594a9f89b855943e5f645f351049e
mmjck <msantosjck@gmail.com>
2024-07-28 12:20:57 -0400

    feat: added address service interface

---
+++ ms/src/main/java/com/carbigdata/ms/service/address/AddressService.java
@@ -0,0 +1,21 @@
+package com.carbigdata.ms.service.address;
+
+import com.carbigdata.ms.domain.address.entities.Address;
+
+public interface AddressService {
+    public Address get(int id);
+
+    public Address update(int id, String state,
+            String city,
+            String zipCode,
+            String district);
+
+    public Address create(String state,
+            String city,
+            String zipCode,
+            String district);
+
+    public void delete(int id);
+}
+
+
